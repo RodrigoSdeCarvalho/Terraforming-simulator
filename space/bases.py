61d31a5ae4ac07afa1857a668104c54011566cc1
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2022-07-11 22:45:31 -0300

    fixed mutex bug and needed vs avaliable resources

--- space/bases.py
+++ space/bases.py
@@ -1,3 +1,4 @@
+from time import sleep
 import globals
 from threading import Thread
 from space.rocket import Rocket
@@ -53,29 +54,41 @@ class SpaceBase(Thread):
         oil_mine = globals.get_mines_ref()["oil_earth"]
         oil_mutex = globals.get_oil_mutex()
 
-        oil_mutex.acquire()
-        if self.mine_has_enough_oil(oil_mine):
+        if self.mine_has_oil(oil_mine):
+            oil_mutex.acquire()
             needed_oil = self.calculate_needed_oil()
-            oil_mine.unities -= needed_oil
-            self.fuel += needed_oil
-            oil_mutex.release()
+
+            if needed_oil > oil_mine.unities:
+                self.fuel += oil_mine.unities
+                oil_mine.unities = 0
+                oil_mutex.release()
+            else:
+                self.fuel += needed_oil
+                oil_mine.unities -= needed_oil
+                oil_mutex.release()
 
     def refuel_uranium(self):
         uranium_mine = globals.get_mines_ref()["uranium_earth"] 
         uranium_mutex = globals.get_uranium_mutex()
 
-        uranium_mutex.acquire()
-        if self.mine_has_enough_uranium(uranium_mine): 
-            needed_uranium = self.calculate_needed_oil()
-            uranium_mine.unities -= needed_uranium
-            self.fuel += needed_uranium
-            uranium_mutex.release()
+        if self.mine_has_uranium(uranium_mine): 
+            uranium_mutex.acquire()
+            needed_uranium = self.calculate_needed_uraninum()
 
-    def mine_has_enough_oil(self, oil_mine):
-        return oil_mine.unities >= self.constraints[1]
-        
-    def mine_has_enough_uranium(self, uranium_mine):
-        return uranium_mine.unities >= self.constraints[0]
+            if needed_uranium > uranium_mine.unities:
+                self.uranium += uranium_mine.unities
+                uranium_mine.unities = 0
+                uranium_mutex.release()
+            else:
+                self.uranium += needed_uranium
+                uranium_mine.unities -= needed_uranium
+                uranium_mutex.release()
+
+    def mine_has_oil(self, oil_mine):
+        return oil_mine.unities > 0
+
+    def mine_has_uranium(self, uranium_mine):
+        return uranium_mine.unities > 0
 
     def calculate_needed_oil(self):
         needed_oil = self.constraints[1] - self.fuel
@@ -99,9 +112,11 @@ class SpaceBase(Thread):
 
             if not self.base_has_full_uranium():
                 self.refuel_uranium()
+                
+            #lancar_foguete()
             
     def base_has_full_oil(self):
-        return self.fuel == self.constraints[1]
+        return self.fuel >= self.constraints[1]
 
     def base_has_full_uranium(self):
-        return self.uranium == self.constraints[0]
+        return self.uranium >= self.constraints[0]
